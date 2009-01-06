3425245ad7f563066d381146275c90e2c2fef3ca
Paul Legato <pjlegato@gmail.com>
2009-01-06 01:28:09 -0500

    Add missing "do".

--- lib/vlad/maintenance.rb
+++ lib/vlad/maintenance.rb
@@ -1,14 +1,14 @@
-namespace :vlad
+namespace :vlad do
   desc "Remove the maintenance page"
   remote_task :start_web, :roles => [:web] do
     run "if [ -f #{shared_path}/system/maintenance.html ]; then rm -f #{shared_path}/system/maintenance.html; fi"
   end
-  
+
   desc "Put the maintenance page in place"
   remote_task :stop_web, :roles => [:web] do
     run "cp -f #{shared_path}/config/maintenance.html #{shared_path}/system/"
   end
-  
+
   desc "(Re)Start the web and app servers"
   remote_task :start do
     Rake::Task['vlad:start_app'].invoke
